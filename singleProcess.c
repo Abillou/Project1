071eebb7b8a5ec423123a7393a7b1a8d95ef826f
Abillou <abillou59@gmail.com>
2023-03-28 23:36:15 -0400

    Text File Generation Added

--- singleProcess.c
+++ singleProcess.c
@@ -6,23 +6,64 @@
 #include <stdarg.h>
 #include <signal.h>
 #include <time.h>  
-
+#include <stdbool.h>
 int L, H, PN;
 
 void generateTextFile(){
+    //Initializes Array of Locations
+    int loc[L+50];
+    for(int i = 0; i < L+50; i++)
+        loc[i] = 0;
     
+    int count = 0;
+    srand(time(NULL));
+    int randomNumber;
+    
+    //Picks random indexs to be -1 (No Duplicates)
+    while(count < 50){
+        randomNumber = rand() % (L+50);
+        if (loc[randomNumber] == 0){
+            loc[randomNumber] = -1;
+            count++;
+            
+        } 
+    } 
+
+    //Creates/Overwrites a text file name keys
+    FILE *keys;
+    keys = fopen("keys.txt", "w");
+
+    
+    for(int i = 0; i < (L+50); i++){
+        //Generates a random positive integer. 
+        randomNumber = rand(); 
+
+        //If the corresponding index to the array is -1, place -1 in the file
+        if(loc[i] == -1){
+            fputs("-1\n", keys);
+        }
+
+        //Otherwise, place the randomly generated number.
+        else
+            fprintf(keys, "%d\n", randomNumber);      
+    }
+
+    //Close file.
+    fclose(keys);
+ 
 }
 
 int main(int argc, char* argv[]){
     if(argc != 4){
         printf("Not enough arguements");
-        return;
+        return -1;
     }
 
     L = atoi(argv[1]);
     H = atoi(argv[2]);
     PN = atoi(argv[3]);
 
+    generateTextFile();
 
 
 }
