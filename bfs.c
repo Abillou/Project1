125a04bd3ddc2ae02ff3f267691ff2c79aa3ee6f
Abillou <abillou59@gmail.com>
2023-04-09 20:40:56 -0400

    Getting somewhere with BFS! -Abid

--- bfs.c
+++ bfs.c
@@ -64,6 +64,7 @@ int main(int argc, char* argv[]){
     H = atoi(argv[2]);
     PN = atoi(argv[3]);
     int maxChildren;
+    int parentRoot = getpid();
     char* input = malloc(50);
     printf("How Many Children (2, 3, 4)?\n");
     scanf("%d",&maxChildren);//get input from user
@@ -85,9 +86,8 @@ int main(int argc, char* argv[]){
         fgets(line, sizeof(line), file);
         array[i] = atoi(line);
     }
-  int result = ceil(log2(PN)); 
+  int result = floor(log2(PN)); 
   int returnArg = 1;
-  printf("Level of tree: %d\n", result);
     
 
     //Makes N number of childs for Parent without Children making Processes.
@@ -96,11 +96,15 @@ int main(int argc, char* argv[]){
 
     pid_t childMaker;
 
-
+    int childCounter=-1;
+    int fd[2*PN];
     int pid;
     int start = 0;
     int end = L+50;
        //START THE BFS TREE
+    int childTrack[4] = {-1, -1, -1, -1};
+    int parentPipe = -1;
+    int oldEnd;
     for (int j=0; j<result; j++) 
     {
         if(j!=0){
@@ -113,20 +117,32 @@ int main(int argc, char* argv[]){
         }
         childMaker = getpid();
         int increments;
-         increments = (end-start)/maxChildren;
+         increments = ceil((end-start)/maxChildren);
+         oldEnd = end;
          end = start;
-         
+         bool found = false;
         for(int i = 0; i < maxChildren; i++){
             
-             
-            
             if(childMaker == getpid() && returnArg < PN){
+                childCounter++;
+                
+                for(int l = 0; l < maxChildren; l++)
+                if(childTrack[l]==-1 ){
+                    childTrack[l] = returnArg-1;
+                    
+                    break;
+                }
+                
+                pipe(&fd[2*(returnArg-1)]);
                 pid = fork();
                 if(i!=0)
                     start = start+increments;
-                end = end+increments;   
+                end = end+increments;
+                if((L+50)-end < 5)
+                end = L+50;   
                 returnArg = returnArg+1;
             }
+ 
             
         }
 
@@ -136,11 +152,36 @@ int main(int argc, char* argv[]){
         //CHILD PROCESS
         else if (pid == 0) {
                 
-                printf("Process %d created by process %d with return code %d\n", getpid(), getppid(), returnArg);
-                printf("My start is %d and my end is %d\n",start,end);
-                pid = getpid();
-                if(j == (result-1))
-                exit(0);
+            
+            for(int l = 0; l < maxChildren; l++){
+            if(childTrack[l]!=-1){
+                parentPipe = childTrack[l];
+                childTrack[l] = -1;
+            }
+            }
+
+            printf("Process %d created by process %d with return code %d\n", getpid(), getppid(), returnArg);
+            pid = getpid();
+            if(j == (result-1)){
+                int max = 0;
+                int64_t avg = 0;
+                int count = 0;
+                for(int j = start; j < end; j++){
+                    if (array[j] > max)
+                        max = array[j];
+                    avg += array[j];
+                        
+                }
+                avg = avg / (end - start);
+                count = end-start;
+                close(fd[2*parentPipe]);
+                write(fd[2*parentPipe+1], &max, sizeof(int));
+                write(fd[2*parentPipe+1], &avg, sizeof(int64_t));
+                write(fd[2*parentPipe+1], &count, sizeof(int));
+                close(fd[2*parentPipe+1]);
+                
+            exit(0);
+            }
 
         
         }
@@ -148,8 +189,104 @@ int main(int argc, char* argv[]){
 
 
         else { // parent process (NO FORKING IN HERE TO KEEP CHAIN FORMAT!) Some ends need to be closed.
+                bool hasChildren = false;
+                int childCount = 0;
+                int max = 0;
+                int64_t avg = 0;
+                int count = 0;
+                int tempMax = -1;
+                int tempAvg = -1;
+                int tempCount = -1;
+                for(int r = 0; r < maxChildren; r++){
+                    if(childTrack[r] != -1){
+                        hasChildren = true;
+                        childCount++;
+                    }
 
+                    else if(childTrack[r] == -1 && hasChildren){
+                        start = end;
+                        end = oldEnd;
+                    }
+                    
+                }
 
+                if(!hasChildren){
+                    end = oldEnd;
+                }
+                
+                if(childCount == 0) //If process with no children end up here
+                {
+                    for(int j = start; j < end; j++){
+                        if (array[j] > max)
+                            max = array[j];
+                        avg += array[j];
+                            
+                    }
+                    avg = avg / (end - start);
+                    count = end-start;
+                }
+
+                else if(childCount < maxChildren) //If a process with not the max children ends up here
+                {
+                    for(int j = start; j < end; j++){
+                        if (array[j] > max)
+                            max = array[j];
+                        avg += array[j];
+                            
+                    }
+                    avg = avg / (end - start);
+                    count = end-start;
+
+                    for(int r = 0; r < childCount; r++){
+                        //PIPE CALLED ONE
+                        read(fd[2*childTrack[r]], &tempMax, sizeof(int));
+                        read(fd[2*childTrack[r]], &tempAvg, sizeof(int64_t));
+                        read(fd[2*childTrack[r]], &tempCount, sizeof(int));
+
+                        if(tempMax >= max){
+                            max = tempMax;
+                        }
+                        avg = (avg*count + tempAvg * tempCount)/(tempCount+count);
+                        count += tempCount; 
+                    }
+
+                }
+
+                else{
+                    for(int r = 0; r < childCount; r++){
+                        //PIPE CALLED TWO
+                        if(r!=0){
+                        read(fd[2*childTrack[r]], &tempMax, sizeof(int));
+                        read(fd[2*childTrack[r]], &tempAvg, sizeof(int64_t));
+                        read(fd[2*childTrack[r]], &tempCount, sizeof(int));
+                        if(tempMax >= max){
+                            max = tempMax;
+                        }
+                       avg = (avg*count + tempAvg * tempCount)/(tempCount+count); 
+                        }
+                        else{
+                            read(fd[2*childTrack[r]], &max, sizeof(int));
+                            read(fd[2*childTrack[r]], &avg, sizeof(int64_t));
+                            read(fd[2*childTrack[r]], &count, sizeof(int));
+                        }
+                        
+                    }
+                }
+
+                if(parentRoot != getpid()){
+
+                    //PIPE CALLED THREE
+                    write(fd[2*parentPipe+1], &max, sizeof(int));
+                    write(fd[2*parentPipe+1], &avg, sizeof(int64_t));
+                    write(fd[2*parentPipe+1], &count, sizeof(int));
+                }
+
+                else{
+                    wait(NULL);
+
+                    printf("Max: %d, Avg: %ld\n", max, avg);
+                }
+                //printf("Parent Process %d: My start is %d and my end is %d\n", getpid(), start, end);
                 wait(NULL);
                 exit(0);
             }
